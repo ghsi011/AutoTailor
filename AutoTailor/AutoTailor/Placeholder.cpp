e9c20ff7e00d8d6c13937e226a0744a0c9d0c89e
idan <ghsi011@gmail.com>
2020-12-08 14:25:02 +0200

    compiles

---
+++ AutoTailor/AutoTailor/Placeholder.cpp
@@ -0,0 +1,8 @@
+﻿#include "Placeholder.hpp"
+
+#include <iostream>
+
+void Placeholder::run()
+{
+	std::wcout << "ran\n";
+}
---
+++ AutoTailor/AutoTailor/Placeholder.hpp
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+class Placeholder final
+{
+public:
+	static void run();
+
+public:
+	explicit Placeholder() = delete;
+	~Placeholder() = delete;
+	Placeholder(const Placeholder&) = delete;
+	Placeholder& operator=(const Placeholder&) = delete;	
+};
